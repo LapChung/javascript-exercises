e9b15dfa6581d8efc9dba5f0150445399092cbb1
Lap Chung <lapchung12@gmail.com>
2022-07-04 16:59:45 -0400

    Problems 1-7 solutions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (x) {
+  let converted = (x - 32) * (5 / 9);
+  return parseFloat(converted.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (x) {
+  let converted = x * (9 / 5) + 32;
+  return parseFloat(converted.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
